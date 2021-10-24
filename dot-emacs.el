10c05a40462670146b2e9a026b839e610af829b0
Ricardo Bánffy <rbanffy@gmail.com>
2021-10-24 13:56:20 +0100

    Move ido, fci into custom block, use display-line-numbers instead of linum

--- dot-emacs.el
+++ dot-emacs.el
@@ -14,25 +14,25 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(custom-enabled-themes (quote (misterioso)))
+ '(custom-enabled-themes '(misterioso))
  '(debug-on-error t)
+ '(global-display-fill-column-indicator-mode t)
+ '(global-display-line-numbers-mode t)
+ '(ido-mode 'both nil (ido))
  '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
  '(package-selected-packages
-   (quote
-    (go-mode gradle-mode groovy-mode fill-column-indicator cobol-mode markdown-mode anaconda-mode ansi ansible selectric-mode terraform-mode pyvenv stripe-buffer))))
+   '(go-mode gradle-mode groovy-mode fill-column-indicator cobol-mode markdown-mode anaconda-mode ansi ansible selectric-mode terraform-mode pyvenv stripe-buffer))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#2d3743" :foreground "#e1e1e0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 190 :width normal :foundry "nil" :family "IBM 3270"))))
- '(fixed-pitch ((t (:family "ibm 3270")))))
-
-;; Enable ido-mode
-(ido-mode)
-
-;; Enable fill-column-indicator
-(fci-mode)
+ '(default ((t (:inherit nil :stipple nil :background "#2d3743" :foreground "#e1e1e0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "IBM 3270"))))
+ '(fixed-pitch ((t (:family "IBM 3270"))))
+ '(line-number ((t (:inherit shadow :family "IBM 3270 Narrow"))))
+ '(line-number-current-line ((t (:inherit line-number :background "gray33" :foreground "gold")))))
 
 ;; Remove trailing whitespace on save
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
