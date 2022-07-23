50bd4b05d2af9c7601eda231022216978ed7369b
Ricardo Bánffy <rbanffy@gmail.com>
2022-07-23 21:40:29 +0100

    Enable a couple modules

--- dot-emacs.el
+++ dot-emacs.el
@@ -23,7 +23,7 @@
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
  '(package-selected-packages
-   '(go-mode gradle-mode groovy-mode fill-column-indicator cobol-mode markdown-mode anaconda-mode ansi ansible selectric-mode terraform-mode pyvenv stripe-buffer))
+   '(magit magit-todos yaml-mode go-mode gradle-mode groovy-mode fill-column-indicator cobol-mode markdown-mode anaconda-mode ansi ansible selectric-mode terraform-mode pyvenv stripe-buffer))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
