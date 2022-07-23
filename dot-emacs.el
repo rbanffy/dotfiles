3d75b398c87ba4d73583c2bef71f00adb26dc17f
Ricardo Bánffy <rbanffy@gmail.com>
2022-07-23 21:40:53 +0100

    Make the screen font slightly smaller

--- dot-emacs.el
+++ dot-emacs.el
@@ -30,7 +30,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#2d3743" :foreground "#e1e1e0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "IBM 3270"))))
+ '(default ((t (:inherit nil :stipple nil :background "#2d3743" :foreground "#e1e1e0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "IBM 3270"))))
  '(fixed-pitch ((t (:family "IBM 3270"))))
  '(line-number ((t (:inherit shadow :family "IBM 3270 Narrow"))))
  '(line-number-current-line ((t (:inherit line-number :background "gray33" :foreground "gold")))))
